1d05c6282ec041b9e8d6e71954feb94d65846d2c
ErwannLG <e.legall@codeur.online>
2019-10-10 11:52:03 +0200

    adding use validator constraints file

--- src/Form/TransferType.php
+++ src/Form/TransferType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Transfer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class TransferType extends AbstractType
 {
